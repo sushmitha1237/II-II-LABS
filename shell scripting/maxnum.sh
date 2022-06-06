a2fb10d6098e9a179922d90c90ebb21269514641
sushmitha1237 <96813991+sushmitha1237@users.noreply.github.com>
2022-06-06 11:28:42 +0530

    Create maxnum.sh

---
+++ shell scripting/maxnum.sh
@@ -0,0 +1,12 @@
+echo "Enter three values"
+read a
+read b
+read c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then 
+	echo "$a is greatest"
+elif [ $b -gt $a ] && [ $b -gt $c ]
+then
+	echo "$b is greatest"
+else
+	echo "$c is greatest
